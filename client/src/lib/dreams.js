97552c69b0d13df329ffb40353a3b94f04e5b20a
Luis Molinuevo <89353175+luismolinuevo@users.noreply.github.com>
2024-04-20 17:15:15 -0400

    called updatePost on client

--- client/src/lib/dreams.js
+++ client/src/lib/dreams.js
@@ -1,34 +1,69 @@
 export const createDream = async (data) => {
-    try {
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/dream`,
-        {
-          method: "POST",
-          headers: {
-            "content-type": "application/json",
-            // Authorization: `Bearer ${token}`,
-          },
-          body: JSON.stringify(data),
-        }
-      );
-  
-      if (!response.ok) {
-        console.error(
-          "Create dream request request failed. Response status:",
-          response.status
-        );
-        const errorData = await response.json();
-        console.error("Error details:", errorData);
-  
-        throw new Error("Create dream request failed");
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/dream`,
+      {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+          // Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(data),
       }
-  
-      // If the response is successful, return the data (if needed)
-      const responseData = await response.json();
-      return responseData;
-    } catch (error) {
-      // Log more details about the error
-      console.error("Error during creating dream:", error);
-      throw error;
+    );
+
+    if (!response.ok) {
+      console.error(
+        "Create dream request request failed. Response status:",
+        response.status
+      );
+      const errorData = await response.json();
+      console.error("Error details:", errorData);
+
+      throw new Error("Create dream request failed");
     }
-  };
+
+    // If the response is successful, return the data (if needed)
+    const responseData = await response.json();
+    return responseData;
+  } catch (error) {
+    // Log more details about the error
+    console.error("Error during creating dream:", error);
+    throw error;
+  }
+};
+
+export const updatePost = async (data, dream_id) => {
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/dreams/${dream_id}`,
+      {
+        method: "PUT",
+        headers: {
+          "content-type": "application/json",
+        //   Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(data),
+      }
+    );
+
+    if (!response.ok) {
+      console.error(
+        "Edit dream request request failed. Response status:",
+        response.status
+      );
+      const errorData = await response.json();
+      console.error("Error details:", errorData);
+
+      throw new Error("Edit dream request failed");
+    }
+
+    // If the response is successful, return the data (if needed)
+    const responseData = await response.json();
+    return responseData;
+  } catch (error) {
+    // Log more details about the error
+    console.error("Error during editing dream:", error);
+    throw error;
+  }
+};
