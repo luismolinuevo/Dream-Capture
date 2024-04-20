f5f18563c77ddd3bf49cd0ebc678a1fa07f9cadb
Luis Molinuevo <89353175+luismolinuevo@users.noreply.github.com>
2024-04-20 12:55:12 -0400

    basic server working

--- server/server.js
+++ server/server.js
@@ -1,5 +1,12 @@
 import express from "express";
-
-
+import morgan from "morgan";
 
 const app = express();
+
+app.use(express.json());
+app.use(morgan("tiny"))
+
+
+app.listen(3002, () => {
+    console.log("Connected to server")
+})
